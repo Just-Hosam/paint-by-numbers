060a2c7ddbf367434733b2a78e33bb4de3d2a459
Just-Hosam <hosam_firas@hotmail.com>
2025-05-26 15:15:44 -0600

    Update App comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { NavLink } from "react-router"
+import "./App.css"
+import { Button } from "@mui/material"
 
-function App() {
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Find my Numbers</h1>
+      <NavLink to="/find">
+        <Button>Take Photo</Button>
+      </NavLink>
     </div>
-  );
+  )
 }
 
-export default App;
+export function Find() {
+  return (
+    <div className="Find">
+      <h1>Find Numbers</h1>
+      <p>This is the Find Numbers page.</p>
+    </div>
+  )
+}
